f5647292e2db803c592d71ee5cbfc786bc987ebd
Bogdan Bardi <bogdan.bardi@digilent.ro>
2020-09-25 16:11:33 +0300

    Import commit
    This commit is the import of the HW from https://github.com/Digilent/Zybo-Z7-20-base-linux for the Vivado 2020.1 updated version.
    The previous repository will be kept for older versions, but this branch will be used for new updates moving forward
    aaaaa

---
+++ project_info.tcl
@@ -0,0 +1,24 @@
+# This is an automatically generated file used by digilent_vivado_checkout.tcl to set project options
+proc set_project_properties_post_create_project {proj_name} {
+    set project_obj [get_projects $proj_name]
+    set_property "part" "xc7z020clg400-1" $project_obj
+    set_property "board_part" "digilentinc.com:zybo-z7-20:part0:1.0" $project_obj
+    set_property "default_lib" "xil_defaultlib" $project_obj
+    set_property "simulator_language" "Mixed" $project_obj
+    set_property "target_language" "VHDL" $project_obj
+}
+
+proc set_project_properties_pre_add_repo {proj_name} {
+    set project_obj [get_projects $proj_name]
+    # default nothing
+}
+
+proc set_project_properties_post_create_runs {proj_name} {
+    set project_obj [get_projects $proj_name]
+	#Custom directives for synthesis and implementation
+	set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE ExtraTimingOpt [get_runs impl_1]
+	set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE Explore [get_runs impl_1]
+	set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE RuntimeOptimized [get_runs impl_1]
+	set_property STEPS.WRITE_BITSTREAM.ARGS.READBACK_FILE 0 [get_runs impl_1]
+	set_property STEPS.WRITE_BITSTREAM.ARGS.VERBOSE 0 [get_runs impl_1]
+}
